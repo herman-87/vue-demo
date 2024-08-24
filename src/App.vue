3a783c16d46d6dd50dd6f32c38b2067e27cb9f90
Essoungou Herman <89879807+herman-87@users.noreply.github.com>
2024-08-24 14:22:12 +0100

    vue-project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <HelloWord />
+    <p>{{ sentence }}</p>
+
+    <input type="text" v-model="name">
+    <input type="text" v-model="age">
+</template>
+
+<script setup>
+import { watch, ref } from "vue";
+import HelloWord from "./components/HelloWorld.vue";
+
+const name = ref('herman');
+const age = ref(10);
+watch(name, (newValue, oldValue) => {
+    console.log(`new value: ${newValue} | old value: ${oldValue}`);
+}) 
+</script>
+
+<style scoped>
+.red{
+    color: red;
+}
+.green{
+    color: green;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Hello World</h1>
+    <slot name="name"></slot>
+    <slot name="age"></slot>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
